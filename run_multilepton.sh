d2311a46816ed2bb6807aa88901c1bd59f8ee355
Khawla Jaffel <khawla.jaffel@cern.ch>
2025-10-25 12:10:27 +0300

    trying different dataset

--- run_multilepton.sh
+++ run_multilepton.sh
@@ -2,7 +2,7 @@ law run cf.PlotVariables1D \
     --version bla4 \
     --producers default \
     --variables nmu \
-    --datasets qcd \
+    --datasets dy_m50toinf_amcatnlo \
     --view-cmd imgcat \
     --configs 23preBPix_v12_central \
     $1 
